5ff0a47dde28d12522695a8e747ed04ca3dbec45
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-04-15 16:29:06 -0700

    query 1 sql - employee detail

---
+++ EmployeeSQL/EmployeeDetail_1.sql
@@ -0,0 +1,6 @@
+--List employee details: employee numbers, last name, first name, sex, and salary
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no
+;
