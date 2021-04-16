646d873900f6210b6fd436eb75cf3839305144a9
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-04-16 01:34:45 -0700

    update: query 4 - dept_employee

--- EmployeeSQL/DeptEmployee_4.sql
+++ EmployeeSQL/DeptEmployee_4.sql
@@ -1,13 +1,4 @@
 --List department of each employee: employee no.,last name, first name, department name
-SELECT * FROM employees
-;
-
-SELECT * FROM departments
-;
-
-SELECT * FROM dept_emp
-;
-
 SELECT e.emp_no AS employee_number, e.last_name, e.first_name, d.dept_name AS department_name
 FROM employees AS e
 	LEFT JOIN dept_emp 
