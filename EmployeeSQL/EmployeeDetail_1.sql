93688066b4c865807a9e3cad48393888e8bd3615
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-04-15 18:00:02 -0700

    query 3 sql - manager detail

--- EmployeeSQL/EmployeeDetail_1.sql
+++ EmployeeSQL/EmployeeDetail_1.sql
@@ -1,4 +1,4 @@
---List employee details: employee numbers, last name, first name, sex, and salary
+--List employee details: employee no., last name, first name, sex, and salary
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON
---
+++ EmployeeSQL/ManagerDetail_3.sql
@@ -0,0 +1,9 @@
+--List manager details: department no., department name, employee no., last name, first name
+SELECT * FROM dept_manager
+;
+
+SELECT * FROM departments
+;
+
+SELECT * FROM employees
+;
