fb87f8ed0114761f6a6492b565a4a14ee29c0487
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-04-16 01:16:20 -0700

    update: query 3 sql - manager detail

--- EmployeeSQL/ManagerDetail_3.sql
+++ EmployeeSQL/ManagerDetail_3.sql
@@ -1,9 +1,9 @@
 --List manager details: department no., department name, employee no., last name, first name
-SELECT * FROM dept_manager
-;
-
-SELECT * FROM departments
-;
-
-SELECT * FROM employees
+SELECT d.dept_no AS department_number, d.dept_name AS department_name,
+dm.emp_no AS employee_number, e.last_name, e.first_name
+FROM departments AS d
+	LEFT JOIN dept_manager AS dm
+		USING(dept_no)
+	LEFT JOIN employees AS e
+		USING(emp_no)
 ;
