d101e1391e928e25d65df67e77410d29fded0e0a
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-04-16 19:20:03 -0700

    query 5 sql - Hercules

---
+++ EmployeeSQL/HerculesName_5.sql
@@ -0,0 +1,8 @@
+--List employees by first name 'Hercules' AND last name starts with 'B'
+SELECT E.first_name, E.last_name, E.sex
+	FROM employees AS E
+		WHERE first_name = 'Hercules'
+		AND last_name LIKE 'B%'
+	ORDER BY last_name ASC
+;
+	
